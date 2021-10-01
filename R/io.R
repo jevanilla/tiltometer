7f4a8cc451f22c8848ed5f558ba808939034e673
rfrancolini <rene.francolini@gmail.com>
2021-10-01 10:10:58 -0400

    add deploy and recover date options

--- R/io.R
+++ R/io.R
@@ -7,13 +7,40 @@ example_filename <- function(){
               package="tiltometer")
 }
 
+#' clip tiltometer table by date
+#'
+#' @export
+#' @param x tibble, tiltometer
+#' @param deploy POSIXt or NA, if not NA, clip data before this time
+#' @param recover POSIXt or NA, if not NA, clip data before this time
+#' @return tibble
+clip_tiltometer <- function(x,
+                            deploy = NA,
+                            recover = NA) {
+
+  if (!is.na(deploy)) {
+    x <- x %>%
+      dplyr::filter(date >= deploy[1])
+  }
+
+  if (!is.na(recover)) {
+    x <- x %>%
+      dplyr::filter(date <= recover[1])
+  }
+
+  x
+}
 
 #' read tiltometer data file
 #'
 #' @export
 #' @param filename character, the name of the file
+#' @param deploy POSIXt or NA, if not NA, clip data before this time
+#' @param recover POSIXt or NA, if not NA, clip data before this time
 #' @return tibble
-read_tiltometer <- function(filename = example_filename()){
+read_tiltometer <- function(filename = example_filename(),
+                            deploy = NA,
+                            recover = NA){
   stopifnot(inherits(filename, "character"))
   stopifnot(file.exists(filename[1]))
   x <- suppressMessages(readr::read_csv(filename[1]))
@@ -30,6 +57,12 @@ read_tiltometer <- function(filename = example_filename()){
   attr(x, "filename") <- filename[1]
   #use the spec attr for original colnames
   #attr(x, "original_colnames") <- h
+
+
+  x <- clip_tiltometer(x,
+                       deploy = deploy,
+                       recover = recover)
+
   return(x)
 
 }
